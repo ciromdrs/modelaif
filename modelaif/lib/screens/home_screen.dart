8356bbc9903da3415d72ea0310fa595adaa071b6
Arthur Barros <mb.arthurmiguel@gmail.com>
2026-09-23 21:51:37 -0300

    fix: altera a barra inferior para um menu flutuante.

--- modelaif/lib/screens/home_screen.dart
+++ modelaif/lib/screens/home_screen.dart
@@ -25,10 +25,12 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     final theme = Theme.of(context);
+    final mediaQuery = MediaQuery.of(context);
     
     return Scaffold(
       key: _scaffoldKey,
       backgroundColor: theme.colorScheme.surface,
+      extendBody: true,
 
       // DRAWER (Menu Lateral de Hambúrguer)
       drawer: const Drawer(
@@ -134,28 +136,45 @@ class _HomeScreenState extends State<HomeScreen> {
       ),
 
       // BOTTOM NAVIGATION BAR (Barra Inferior Fixa)
-      bottomNavigationBar: NavigationBarTheme(
-        data: NavigationBarThemeData(),
-        child: NavigationBar(
-          selectedIndex: _selectedIndex,
-          onDestinationSelected: (int index) {
-            setState(() {
-              _selectedIndex = index;
-            });
-          },
-          elevation: 0,
-          destinations: const [
-            NavigationDestination(
-              icon: Icon(Icons.checkroom_outlined),
-              selectedIcon: Icon(Icons.checkroom),
-              label: '',
-            ),
-            NavigationDestination(
-              icon: Icon(Icons.design_services_outlined),
-              selectedIcon: Icon(Icons.design_services),
-              label: '',
-            ),
-          ],
+      bottomNavigationBar: UnconstrainedBox(
+        child: Container(
+          width: mediaQuery.size.width * .48,
+          height: 80,
+          margin: EdgeInsets.only(left: 0, top: 0, right: 0, bottom: 20),
+          decoration: BoxDecoration(
+            color: theme.colorScheme.surfaceContainer,
+            borderRadius: BorderRadius.circular(48),
+            boxShadow: [
+              BoxShadow(
+                color: Color.fromARGB(160, 12, 12, 12),
+                offset: Offset(0, 3),
+                blurRadius: 5
+              )
+            ]
+          ),
+          child: NavigationBar(
+            selectedIndex: _selectedIndex,
+            onDestinationSelected: (int index) {
+              setState(() {
+                _selectedIndex = index;
+              });
+            },
+            backgroundColor: Colors.transparent,
+            elevation: 4,
+            destinations: const [
+              // TODO: criar constante para armazenar o tamanho dos ícones (size)
+              NavigationDestination(
+                icon: Icon(Icons.checkroom_outlined, size: 38),
+                selectedIcon: Icon(Icons.checkroom, size: 38),
+                label: 'Moldes',
+              ),
+              NavigationDestination(
+                icon: Icon(Icons.design_services_outlined, size: 38),
+                selectedIcon: Icon(Icons.design_services, size: 38),
+                label: 'Medidas',
+              ),
+            ]
+          )
         ),
       ),
     );
@@ -205,7 +224,7 @@ class _AnimatedCategoryCardState extends State<_AnimatedCategoryCard> {
                 child: Container(
                   width: double.infinity,
                   decoration: BoxDecoration(
-                    color: theme.colorScheme.surfaceContainer,
+                    color: theme.colorScheme.surfaceContainerLowest,
                     borderRadius: BorderRadius.circular(16),
                   ),
                 ),
